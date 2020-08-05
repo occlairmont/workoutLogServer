bb922bba64fa1df307765ca5a10d79db6f1e6815
occlairmont <occlairmont@gmail.com>
2020-08-05 19:24:37 -0400

    final commit

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ let app = express();
 let sequelize = require("./db");
 let log = require("./controllers/logcontroller");
 let user = require("./controllers/usercontroller");
+app.use(require("./middleware/headers"));
 
 sequelize.sync();
 app.use(express.json());
 app.use("/api", user);
-app.use(require("./middleware/validate-session"));
+// app.use(require("./middleware/validate-session"));
 app.use("/api/log", log);
 
 app.listen(3002, function () {
